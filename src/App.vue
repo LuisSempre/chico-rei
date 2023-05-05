be264d91028ea5909e2202fd1facd53a1df9a70c
LuisSempre <luis.silva@viannasempre.com.br>
2023-05-05 20:16:18 -0300

    Up 2023-05-05 20:16

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import Menu from './components/Menu.vue'
     <Menu />
     <div class="p-8 flex justify-center items-center max-w-6xl mx-auto gap-4">
       <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/about">Selections</RouterLink>
       <RouterLink to="/shopping">Shopping</RouterLink>
     </div>
     <RouterView />
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,23 @@
 <template>
-  <footer class="flex justify-center items-center max-w-6xl mx-auto p-4 ">
-    &copy;
+  <footer class="flex justify-center items-center max-w-6xl mx-auto p-4">
     <small>
+      Copyright &copy;
+      {{ currentYear }}
       Create with 💚 by
-      <a href="https://github.com/LuisSempre">Luis Lucas</a>
+      <a href="https://github.com/LuisSempre">Luis Lucas.</a>
     </small>
   </footer>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      currentYear: ''
+    }
+  },
+  mounted() {
+    const date = new Date()
+    this.currentYear = date.getFullYear()
+  }
+}
+</script>
