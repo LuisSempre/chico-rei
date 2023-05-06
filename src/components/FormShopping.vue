c9736a954fe3845f262764a595450a1dda105518
LuisSempre <luis.silva@viannasempre.com.br>
2023-05-06 18:39:37 -0300

    Up 2023-05-06 18:39

--- src/components/FormShopping.vue
+++ src/components/FormShopping.vue
@@ -1,23 +1,32 @@
 <template>
-  <form @submit.prevent="submitForm" class="flex mx-auto max-w-6xl flex-col space-y-8">
-    <div class="flex flex-col">
-      <label>Telefone</label>
-      <input v-model="formData.name" type="text" class="border-2 border-black"/>
-      <div v-if="formErrors.name">{{ formErrors.name }}</div>
+  <div class="grid max-w-2xl mx-auto grid-cols-2 gap-8">
+    <div>
+      <form @submit.prevent="submitForm" class="flex mx-auto max-w-6xl flex-col space-y-8">
+        <div class="flex flex-col">
+          <label>Email</label>
+          <input v-model="formData.email" type="email" class="border-2 border-black" />
+          <div v-if="formErrors.email">{{ formErrors.email }}</div>
+        </div>
+        <div class="flex flex-col">
+          <label>Telefone</label>
+          <input v-model="formData.name" type="number" class="border-2 border-black" />
+          <div v-if="formErrors.name">{{ formErrors.name }}</div>
+        </div>
+        <div class="flex justify-end flex-col">
+          <button type="submit" class="bg-gray-900 text-white p-2">Fechar pedido</button>
+          <p v-if="message">{{ message }}</p>
+        </div>
+      </form>
     </div>
-    <div class="flex flex-col">
-      <label>Email</label>
-      <input v-model="formData.email" type="email" class="border-2 border-black"/>
-      <div v-if="formErrors.email">{{ formErrors.email }}</div>
+    <div>
+      
     </div>
-    <div class="flex justify-end">
-      <button type="submit" class="bg-gray-900 text-white p-2">Fechar pedido</button>
-      <div v-if="message">{{ message }}</div>
-    </div>
-  </form>
+  </div>
 </template>
 
 <script>
+import Cep from '../components/Cep.vue'
+import Shopping from '../components/Shopping.vue'
 import { z } from 'zod'
 
 export default {
@@ -26,14 +35,14 @@ export default {
     return {
       formData: {
         name: '',
-        email: '',
+        email: ''
       },
       formSchema: z.object({
         name: z
           .string()
           .nonempty('Name is required.')
-          .min(3, 'Name must be at least 3 characters.'),
-        email: z.string().nonempty('Email is required.').email('Invalid email format.'),
+          .min(11, 'Name must be at least 11 characters.'),
+        email: z.string().nonempty('Email is required.').email('Invalid email format.')
       }),
       formErrors: {}
     }
